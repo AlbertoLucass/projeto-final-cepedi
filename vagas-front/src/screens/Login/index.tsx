82e826e49494446f13bd426fb367daa7fd7cdf1b
laizmesquita <laizmesquita@gmail.com>
2024-11-29 17:35:11 -0300

    Conectando API

--- vagas-front/src/screens/Login/index.tsx
+++ vagas-front/src/screens/Login/index.tsx
@@ -1,6 +1,9 @@
+
 import { Image } from 'react-native';
+import { useState } from 'react';
 import { Wrapper,Container, Form, TextContainer, TextBlack, TextLink, TextLinkContainer } from './styles';
 
+import api from '../../services/api';
 
 import BGTop from '../../assets/BGTop.png';
 import Logo from '../../components/Logo';
@@ -8,6 +11,28 @@ import Input from '../../components/Input';
 import { Button } from '../../components/Button';
 
 export default function Login({ navigation }) {
+
+const [email, setEmail] = useState('');
+const [senha, setSenha] = useState('');
+
+const handleLogin = async () => {
+    try{
+        const response = await api.get('/usuarios');
+        const users = response.data;
+
+        const user = users.find(u => u.email === email && u.senha === senha);
+
+        if(user){
+            navigation.navigate('Auth', {screen: 'home'})
+        }else{
+            console.log('Login falhou.')
+        }
+
+    }catch(error){
+        console.log(error);
+    }
+};
+
     return (
         <Wrapper>
             <Image source={BGTop} />
@@ -16,13 +41,23 @@ export default function Login({ navigation }) {
 
                 <Form>
                     <Logo />
-                    <Input label='E-mail' placeholder='digite seu e-mail'/>
-                    <Input label='Senha' placeholder='digite sua senha'/>
+                    <Input
+                        label='E-mail'
+                        placeholder='digite seu e-mail'
+                        value={email}
+                        onChangeText={setEmail}
+                    />
+                    <Input 
+                        label='Senha'
+                        placeholder='digite sua senha'
+                        value={senha}
+                        onChangeText={setSenha}
+                    />
                     <Button 
                     title="Entrar" 
                     noSpacing={true} 
                     variant='primary'
-                    onPress={() => navigation.navigate('Auth', { screen: 'Home' })}
+                    onPress={handleLogin}
                     />
                     <TextContainer>
                         <TextBlack>Não tem uma conta?</TextBlack>
