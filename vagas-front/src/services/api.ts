2a24eee342d783c70d4f9c700e1e8c07f51bb867
AlbertoLucass <trindade_neto@hotmail.com>
2024-12-01 08:56:53 -0300

    feat: habilitar funcionameto em ambos dispositivos

--- vagas-front/src/services/api.ts
+++ vagas-front/src/services/api.ts
@@ -1,7 +1,19 @@
 import axios from "axios";
+import { Platform } from 'react-native';
+
+const getBaseURL = () => {
+  switch (Platform.OS) {
+    case 'android':
+      return 'http://192.168.0.247:3000';
+    case 'ios':
+      return 'http://10.0.2.2:3000';
+    default:
+      return 'http://localhost:3000';
+  }
+};
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.247:3000'
+  baseURL: getBaseURL()
 });
 
 export default api;
